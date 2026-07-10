4eeb0b8830307b95d9a5fb1f80fdd369a1a2386a
Ritik <ritikpal227@gmail.com>
2026-07-10 19:02:41 +0530

    Update NewsApi.jsx

--- src/components/NewsApp/NewsApi.jsx
+++ src/components/NewsApp/NewsApi.jsx
@@ -11,7 +11,7 @@ const NewsApi = () => {
         if (!input || input.trim() === '') return;
         const news = await fetch(`https://api.mediastack.com/v1/news?access_key=3376c4991a739517704bbe02a7db183f&keywords=${input}&languages=${language}`)
         const newsData = await news.json()
-        setNews(newsData)
+        setNews(newsData.data)
       } catch (error) {
         setError(error)
       }
